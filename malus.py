5b79d13c48393874a899078c4bf86f333e3f2ede
SjoerdB93 <info@sjoerd.se>
2022-08-11 15:40:46 +0200

    - Minor code clean up - Added unknown columns to selection

--- malus.py
+++ malus.py
@@ -6,14 +6,12 @@ from pathlib import Path
 import numpy as np
 
 def load_data(self):
-    try:
-        file_path = get_path(self)
+    file_path = get_path(self)
+    if file_path != "":
         self.filename = Path(file_path).name
         data = get_data(file_path)
         self.dataframe = data
         plot_selection(self)
-    except:
-        pass
 
 
 
@@ -40,60 +38,63 @@ def export_data(self):
 
 def plot_selection(self):
     if self.dataframe is not None:
-        selection = str(self.selected_item.currentText())
+        selection = get_selection(self)
         title = f"{selection} - {self.filename}"
-        if selection == "Coil 1 current":
-            selection = "coil1_current"
-            self.type = "coil_current"
-        elif selection == "Coil 2 current":
-            selection = "coil2_current"
-            self.type = "coil_current"
-        elif selection == "Bias Voltage":
-            selection = "bias_voltage"
-            self.type = "voltage"
-        elif selection == "Value 3":
-            selection = "value3"
-        elif selection == "MDX 2 Current":
-            selection = "mdx2_current"
-            self.type = "current"
-        elif selection == "MDX 2 Voltage":
-            selection = "mdx2_voltage"
-            self.type = "voltage"
-        elif selection == "MDX 2 Power":
-            selection = "mdx2_power"
-            self.type = "power"
-        elif selection == "MDX 1 Power":
-            selection = "mdx1_power"
-            self.type = "power"
-        elif selection == "MDX 1 Current":
-            selection = "mdx1_current"
-            self.type = "current"
-        elif selection == "MDX 1 Voltage":
-            selection = "mdx1_voltage"
-            self.type = "voltage"
-        elif selection == "N2 Flow (SCCM)":
-            selection = "n2_flow"
-            self.type = "gas"
-        elif selection == "Ar Flow (SCCM)":
-            selection = "ar_flow"
-            self.type = "gas"
-        elif selection == "Delay per second":
-            selection = "delay_second"
-            self.type = "delay_second"
-        elif selection == "Total delay":
-            selection = "delay_total"
-            self.type = "delay_total"
-        else:
-            selection = "coil1_current"
-        self.selection = selection
         self.plot_figure(title = title, selection=selection)
 
+def get_selection(self):
+    selection = str(self.selected_item.currentText())
+    if selection == "Coil 1 current":
+        selection = "coil1_current"
+        self.type = "coil_current"
+    elif selection == "Coil 2 current":
+        selection = "coil2_current"
+        self.type = "coil_current"
+    elif selection == "Bias Voltage":
+        selection = "bias_voltage"
+        self.type = "voltage"
+    elif selection == "Value 3":
+        selection = "value3"
+    elif selection == "MDX 2 Current":
+        selection = "mdx2_current"
+        self.type = "current"
+    elif selection == "MDX 2 Voltage":
+        selection = "mdx2_voltage"
+        self.type = "voltage"
+    elif selection == "MDX 2 Power":
+        selection = "mdx2_power"
+        self.type = "power"
+    elif selection == "MDX 1 Power":
+        selection = "mdx1_power"
+        self.type = "power"
+    elif selection == "MDX 1 Current":
+        selection = "mdx1_current"
+        self.type = "current"
+    elif selection == "MDX 1 Voltage":
+        selection = "mdx1_voltage"
+        self.type = "voltage"
+    elif selection == "N2 Flow (SCCM)":
+        selection = "n2_flow"
+        self.type = "gas"
+    elif selection == "Ar Flow (SCCM)":
+        selection = "ar_flow"
+        self.type = "gas"
+    elif selection == "Delay per second":
+        selection = "delay_second"
+        self.type = "delay_second"
+    elif selection == "Total delay":
+        selection = "delay_total"
+        self.type = "delay_total"
+    else:
+        self.type = "other"
+    self.selection = selection
+    return selection
 
 def get_data(file_path):
     df = pd.read_csv(file_path, sep="\s+", decimal=",", skiprows=2)
     df.columns = ["time", "coil1_current", "coil2_current", "bias_voltage", "value3", "mdx2_current", "mdx2_power",
                   "mdx2_voltage", "mdx1_current", "value11", "value12", "value13", "value14", "value15", "value17",
-                  "value_18", "value16", "mdx1_power", "mdx1_voltage", "ar_flow", "n2_flow"]
+                  "value18", "value16", "mdx1_power", "mdx1_voltage", "ar_flow", "n2_flow"]
     return df
 
 def define_canvas(self):
@@ -115,9 +116,11 @@ def create_layout(self, canvas, layout):
 
 
 def get_path(self, documenttype="Text file (*.txt);;All Files (*)"):
+    print("Yo")
     dialog = QFileDialog
     options = dialog.Options()
     options |= QFileDialog.DontUseNativeDialog
     path = QFileDialog.getOpenFileName(self, "Open files", "",
                                         documenttype, options=options)[0]
+    print(path)
     return path
